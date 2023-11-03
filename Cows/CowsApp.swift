e68e49885f91852339d30c28590b1b2d197df6f0
Sergey Kykhov <ghrle@mail.ru>
2023-11-03 17:48:19 +0300

    Initial Commit

---
+++ Cows/CowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsApp.swift
+//  Cows
+//
+//  Created by Sergey Kykhov on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
